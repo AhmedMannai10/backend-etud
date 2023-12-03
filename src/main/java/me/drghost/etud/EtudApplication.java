faebd46e768f541c21815971956d8798e0d25669
DrGhost <mannaiahmed7518@gmail.com>
2023-12-03 14:03:49 +0100

    initial commit

---
+++ src/main/java/me/drghost/etud/EtudApplication.java
@@ -0,0 +1,13 @@
+package me.drghost.etud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/drghost/etud/EtudApplicationTests.java
@@ -0,0 +1,13 @@
+package me.drghost.etud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
